2470b34bd1fcc00031e006d9f3b054ce0a022cb5
KevinYu3214 <71300074+KevinYu3214@users.noreply.github.com>
2024-08-19 10:33:08 -0500

    Updates

--- service-worker.js
+++ service-worker.js
@@ -1,24 +1,56 @@
-// public/service-worker.js
+const CACHE_NAME = 'cache-v1';
+const STATIC_ASSETS = [
+  '/',
+  '/index.html',
+  '/static/js/main.js',
+  '/static/css/main.css',
+  '/favicon.ico',
+  '/logo192.png'
+];
 
-self.addEventListener('install', (event) => {
-    console.log('Service worker installing...');
-    event.waitUntil(
-      caches.open('cache-v1').then((cache) => {
-        return cache.addAll([
-          '/',
-          '/index.html',
-          '/static/js/main.js',
-          '/static/css/main.css',
-        ]);
-      })
-    );
-  });
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(STATIC_ASSETS);
+    }).catch(err => console.error('Cache error:', err))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      if (response) {
+        return response;
+      }
+      return fetch(event.request).then(networkResponse => {
+        if (event.request.url.startsWith(self.location.origin)) {
+          return caches.open(CACHE_NAME).then(cache => {
+            cache.put(event.request, networkResponse.clone());
+            return networkResponse;
+          });
+        }
+        return networkResponse;
+      }).catch(() => {
+        if (event.request.mode === 'navigate') {
+          return caches.match('/index.html');
+        }
+      });
+    })
+  );
+});
+
+self.addEventListener('activate', event => {
+  const cacheWhitelist = [CACHE_NAME];
   
-  self.addEventListener('fetch', (event) => {
-    event.respondWith(
-      caches.match(event.request).then((response) => {
-        return response || fetch(event.request);
-      })
-    );
-  });
-  
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (!cacheWhitelist.includes(cacheName)) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
